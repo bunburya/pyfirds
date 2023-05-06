a1607c2f2a43219d8f68863a1d408b36f6f14548
bunburya <dev@bunburya.eu>
2023-05-07 00:44:49 +0100

    initial commit

---
+++ pyfirds/download.py
@@ -0,0 +1,147 @@
+import logging
+import os
+from dataclasses import dataclass
+from datetime import datetime
+from hashlib import file_digest
+from zipfile import ZipFile
+
+import requests
+from dateutil import tz
+from pysolr import Solr
+
+BASE_URL = "https://registers.esma.europa.eu/solr/esma_registers_firds_files/"
+
+logger = logging.getLogger(__name__)
+
+class BadChecksumError(Exception):
+    """An error raised when a downloaded file does not have the expected checksum."""
+    pass
+
+@dataclass
+class FirdsDoc:
+    """A dataclass representing a single document reference returned by searching the FIRDS database.
+
+    :param checksum: MD5 checksum for the file.
+    :param download_link: A URL to download the file from.
+    :param file_id: An ID for the file.
+    :param file_name: The name of the file.
+    :param file_type: The type of the file.
+    :param timestamp: The timestamp of the document.
+    """
+
+    checksum: str
+    download_link: str
+    file_id: str
+    file_name: str
+    file_type: str
+    timestamp: datetime
+
+    @staticmethod
+    def from_dict(d: dict) -> 'FirdsDoc':
+        return FirdsDoc(
+            d['checksum'],
+            d['download_link'],
+            d['id'],
+            d['file_name'],
+            d['file_type'],
+            d['timestamp']
+        )
+
+    def download_zip(self, to_dir: str, overwrite: bool = False, verify: bool = True):
+        """Download this file as a zip file to the specified directory.
+
+        :param to_dir: The directory to download the file to.
+        :param overwrite: Whether to overwrite an existing file if one already exists. Default is False.
+        :param verify: Whether to verify the checksum of the downloaded file. Default is True.
+        :return: The absolute path of the file's location.
+        """
+
+        fpath = os.path.join(to_dir, self.file_name)
+        if os.path.exists(fpath) and not overwrite:
+            raise FileExistsError(fpath)
+
+        fpath_part = f'{fpath}.part'
+
+        with requests.get(self.download_link, stream=True) as r:
+            r.raise_for_status()
+            with open(fpath_part, 'wb') as fd:
+                logger.debug(f'Downloading file from {self.download_link} to {fpath}.')
+                for chunk in r.iter_content(1024 * 8):
+                    fd.write(chunk)
+        os.rename(fpath_part, fpath)
+
+        if verify:
+            logger.debug(f'Verifying checksum of file at {fpath}.')
+            with open(fpath, 'rb') as fd:
+                check = file_digest(fd, 'md5').hexdigest()
+                if check != self.checksum:
+                    raise BadChecksumError(f'File {fpath} has checksum {check}, expected {self.checksum}.')
+
+        return fpath
+
+    def download_xml(self, to_dir: str, overwrite: bool = False, verify: bool = True, delete_zip: bool = True):
+        """Download this file as an XML file to the specified directory.
+
+        :param to_dir: The directory to download the file to. If no directory is specified, a temporary one is used.
+        :param overwrite: Whether to overwrite an existing file if one already exists. Default is False.
+        :param verify: Whether to verify the checksum of the downloaded zip file. Default is True.
+        :param delete_zip: Whether to delete the zip file after its contents have been extracted. Default is True.
+        :return: The absolute path of the file's location.
+        """
+
+        zip_fpath = self.download_zip(to_dir, overwrite=overwrite, verify=verify)
+        with open(zip_fpath, 'rb') as fd:
+            zip_file = ZipFile(fd)
+            xml_fname = zip_file.namelist()[0]
+            xml_fpath = os.path.join(to_dir, xml_fname)
+            if os.path.exists(xml_fpath) and not overwrite:
+                raise FileExistsError(xml_fpath)
+            zip_file.extractall(to_dir)
+
+        if delete_zip:
+            os.remove(zip_fpath)
+
+        return os.path.join(to_dir, xml_fname)
+
+
+
+
+
+
+class FirdsSearch:
+
+    def __init__(self, base_url: str):
+        self.base_url = base_url
+        self.solr = Solr(base_url)
+
+    def search(self, from_time: datetime, to_time: datetime, query: str = '*') -> list[FirdsDoc]:
+        """Search the FIRDS database and return a list of download URLs.
+
+        :param from_time: The start of the period to search.
+        :param to_time: The end date of the period to search.
+        :param query: The query to search. Optional, but can be set to 'FULINS', 'DLTINS' or 'FULCAN' to search for full
+            FIRDS records, delta files or cancellation files, respectively.
+
+        """
+
+        logger.debug(f'Searching FIRDS database for query {query} from {from_time} to {to_time}.')
+
+        start = 0
+        rows = 100
+
+        from_str = from_time.astimezone(tz.UTC).strftime("%Y-%m-%dT%H:%M:%SZ")
+        to_str = to_time.astimezone(tz.UTC).strftime("%Y-%m-%dT%H:%M:%SZ")
+        pub_date_fq = f'publication_date:[{from_str} TO {to_str}]'
+        results = self.solr.search(query, fq=pub_date_fq, start=start, rows=rows)
+        docs = [FirdsDoc.from_dict(d) for d in results.docs]
+        hits = results.hits
+        while hits > (start + rows):
+            start += rows
+            results = self.solr.search(query, fq=pub_date_fq, start=start, rows=rows)
+            docs.extend(FirdsDoc.from_dict(d) for d in results.docs)
+        return docs
+
+
+@dataclass
+class FirdsDocName:
+    doc_type: str
---
+++ test/values.py
@@ -0,0 +1,14 @@
+import os
+from shutil import rmtree
+
+TEST_DATA_DIR = os.path.join('test_data')
+TEST_RUN_BASE_DIR = os.path.join(TEST_DATA_DIR, 'run')
+
+if not os.path.exists(TEST_RUN_BASE_DIR):
+    os.makedirs(TEST_RUN_BASE_DIR)
+
+def get_test_run_dir(name: str) -> str:
+    dir_path = os.path.join(TEST_RUN_BASE_DIR, name)
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+    return dir_path
