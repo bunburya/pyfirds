fc201057d310f100b3797575bd2f75834c771f2b
bunburya <dev@bunburya.eu>
2023-10-03 00:20:32 +0100

    further work on DAO

--- pyfirds/db/dao.py
+++ pyfirds/db/dao.py
@@ -1,19 +1,16 @@
 import logging
 from abc import ABC, abstractmethod
-from typing import Any, Callable
+from typing import Any, Callable, Optional
 
-from sqlalchemy import Engine, insert, select, Connection
+from sqlalchemy import Engine, insert, select, Connection, Column, Table
 
-from pyfirds.db.schema import publication_period, technical_attributes, index_term
-from pyfirds.model import ReferenceData, PublicationPeriod, TechnicalAttributes, IndexTerm, Index
+from pyfirds.db.schema import publication_period, technical_attributes, index_term, index, interest_rate, \
+    debt_attributes, strike_price, commodity_derivative_attributes, ir_derivative_attributes
+from pyfirds.model import ReferenceData, PublicationPeriod, TechnicalAttributes, IndexTerm, Index, InterestRate, \
+    DebtAttributes, StrikePrice, CommodityDerivativeAttributes, InterestRateDerivativeAttributes
 
 logger = logging.getLogger(__name__)
 
-
-_add_data_method = Callable[["AbstractFirdsDao", Any, Connection], int]
-def unique(method: _add_data_method) -> _add_data_method:
-    """Wrap a method of :class:`FirdsDao` used to add data to the database"""
-    def f(self, data, conn):
 
 class AbstractFirdsDao(ABC):
 
@@ -37,8 +34,8 @@ class AbstractFirdsDao(ABC):
         """
         raise NotImplementedError
 
-class FirdsDao(AbstractFirdsDao):
 
+class FirdsDao(AbstractFirdsDao):
 
     def add_reference_data(self, data: ReferenceData, commit: bool = False) -> int:
         raise NotImplementedError
@@ -57,9 +54,33 @@ class FirdsDao(AbstractFirdsDao):
         )
         return conn.execute(stmt).inserted_primary_key[0]
 
-    # TODO: Make check for existing index/term and return ID if present
+    def _search_ids(self, table: Table, match: dict[str, Any], conn: Connection) -> list[int]:
+        """Search for all rows matching certain criteria.
+
+        :param table: The table to search.
+        :param match: A dict mapping column names to values to match against.
+        :return: A list of row IDs for matching rows.
+        """
+        stmt = select(table).filter_by(**match)
+        results = conn.execute(stmt).all()
+        return [r.id for r in results]
+
+    def _search_id(self, table: Table, match: dict[str, Any], conn: Connection) -> Optional[int]:
+        """Like `_search_ids`, but only returns the row id for the first matching row, or None if no matching row is
+        found.
+        """
+        ids = self._search_ids(table, match, conn)
+        if not ids:
+            return None
+        if (c := len(ids)) > 1:
+            logger.warning(f"Found {c} matching rows in table `{table}` for query `{match}`.")
+        return ids[0]
+
     def _add_index_term(self, data: IndexTerm, conn: Connection) -> int:
-        """Add an :class:`IndexTerm` to the database and return its id."""
+        """Add an :class:`IndexTerm` to the database (if not already present) and return the row id."""
+        existing = self._search_id(index_term, {"number": data.number, "unit": data.unit}, conn)
+        if existing is not None:
+            return existing
         stmt = insert(index_term).values(
             number=data.number,
             unit=data.unit
@@ -67,10 +88,115 @@ class FirdsDao(AbstractFirdsDao):
         return conn.execute(stmt).inserted_primary_key[0]
 
     def _add_index(self, data: Index, conn: Connection) -> int:
-        """Add an :class:`Index` to the database and return its id."""
-        stmt = insert(index_term).values(
+        """Add an :class:`Index` to the database (if not already present) and return the row id."""
+        existing_term_id = self._search_id(index_term, {"number": data.term.number, "unit": data.term.unit}, conn)
+        existing = self._search_id(index, {"name": data.name, "isin": data.isin, "term_id": existing_term_id}, conn)
+        if existing is not None:
+            return existing
+
+        # If we found an existing ID for the term above, reuse it rather than searching again
+        if existing_term_id is not None:
+            term_id = existing_term_id
+        else:
+            term_id = self._add_index_term(data.term, conn)
+
+        stmt = insert(index).values(
             name=data.name,
             isin=data.isin,
-            term_id=self._add_index_term()
+            term_id=term_id
         )
         return conn.execute(stmt).inserted_primary_key[0]
+
+    def _add_interest_rate(self, data: InterestRate, conn: Connection) -> int:
+        """Add an :class:`InterestRate` to the database (if not already present) and return the row id."""
+        existing_benchmark_id = self._search_id(
+            index,
+            {
+                "name": data.benchmark.name,
+                "isin": data.benchmark.isin,
+                "term_id": self._search_id(
+                    index_term,
+                    {"number": data.benchmark.term.number, "unit": data.benchmark.term.unit},
+                    conn
+                )
+            },
+            conn
+        )
+        existing = self._search_id(
+            interest_rate,
+            {
+                "fixed_rate": data.fixed_rate,
+                "benchmark_id": existing_benchmark_id,
+                "spread": data.spread
+            },
+            conn
+        )
+        if existing is not None:
+            return existing
+
+        # If we found an existing ID for the benchmark above, reuse it rather than searching again
+        if existing_benchmark_id is not None:
+            benchmark_id = existing_benchmark_id
+        else:
+            benchmark_id = self._add_index(data.benchmark, conn)
+
+        stmt = insert(index).values(
+            fixed_rate=data.fixed_rate,
+            benchmark_id=benchmark_id,
+            spread=data.spread
+        )
+        return conn.execute(stmt).inserted_primary_key[0]
+
+    def _add_debt_attrs(self, data: DebtAttributes, conn: Connection) -> int:
+        """Add :class:`DebtAttributes` to the database and return the row id."""
+        stmt = insert(debt_attributes).values(
+            total_issued_amount=data.total_issued_amount,
+            maturity_date=data.maturity_date,
+            nominal_currency=data.nominal_currency,
+            nominal_value_per_unit=data.nominal_value_per_unit,
+            interest_rate_id=self._add_interest_rate(data.interest_rate, conn),
+            seniority=data.seniority
+        )
+        return conn.execute(stmt).inserted_primary_key[0]
+
+    def _add_strike_price(self, data: StrikePrice, conn: Connection) -> int:
+        """Add :class:`StrikePrice` to the database and return the row id."""
+
+        stmt = insert(strike_price).values(
+            type=data.price_type,
+            price=data.price,
+            pending=data.pending,
+            currency=data.currency
+        )
+        return conn.execute(stmt).inserted_primary_key[0]
+
+    def _add_commodity_deriv_attrs(self, data: CommodityDerivativeAttributes, conn: Connection) -> int:
+        """Add :class:`CommodityDerivativeAttributes` to the database and return the row id."""
+
+        stmt = insert(commodity_derivative_attributes).values(
+            base_product=data.base_product,
+            sub_product=data.sub_product,
+            transaction_type=data.transaction_type,
+            final_price_type=data.final_price_type
+        )
+        return conn.execute(stmt).inserted_primary_key[0]
+
+    def _add_ir_deriv_attrs(self, data: InterestRateDerivativeAttributes, conn: Connection) -> int:
+        """Add :class:`InterestRateDerivativeAttributes` to the database and return the row id."""
+
+        stmt = insert(ir_derivative_attributes).values(
+            reference_rate_id=self._add_index(data.reference_rate, conn),
+            notional_currency_2=data.notional_currency_2,
+            fixed_rate_1=data.fixed_rate_1,
+            fixed_rate_2=data.fixed_rate_2,
+            floating_rate_2_id=self._add_index(data.floating_rate_2, conn)
+        )
+        return conn.execute(stmt).inserted_primary_key[0]
+
+
+"""    Column("reference_rate_id", Integer, ForeignKey("index.id"), nullable=False),
+    Column("notional_currency_2", String(3)),
+    Column("fixed_rate_1", Float),
+    Column("fixed_rate_2", Float),
+    Column("floating_rate_2_id", Integer, ForeignKey("index.id"))
+"""
